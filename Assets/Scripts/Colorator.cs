b942965ea85b1b8c29f0c05c2e468e36e3c8c22e
Kate Hinkel <hinckel.katia@gmail.com>
2018-10-19 00:55:42 +0300

    Hello World!!1!

---
+++ Assets/Scripts/Colorator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colorator : MonoBehaviour {
+
+	private Renderer rendSelf;
+
+	// Use this for initialization
+	void Start () {
+		rendSelf = GetComponent<Renderer>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Kek")) {
+			Renderer renOther = other.gameObject.GetComponent<Renderer>();
+			rendSelf.material.color = renOther.material.color;
+			other.gameObject.SetActive(false);
+		}
+	}
+}
